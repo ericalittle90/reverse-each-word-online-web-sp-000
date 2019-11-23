bc16b3f6842d1cb4d4912672a15c1959becdff8f
Erica Little <eteney90@gmail.com>
2019-11-23 20:56:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(array)
-  array.split("").collect do |array|
+  new_array = array.split("").collect do |array|
      array.reverse
   end
-  array.join("")
+  new_array.join("")
 end
